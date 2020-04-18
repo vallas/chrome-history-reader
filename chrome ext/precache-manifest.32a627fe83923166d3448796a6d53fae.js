9b8167d355a1005de5d485064692dbad74e79f29
Core <31855344+vallas@users.noreply.github.com>
2020-04-18 13:20:48 +0200

    Add files via upload

---
+++ chrome ext/precache-manifest.32a627fe83923166d3448796a6d53fae.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a6d2be3cee08ac8d4865b326dc067e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "251906cb07d00d841239",
+    "url": "/static/css/app.css"
+  },
+  {
+    "revision": "23576bd9f2adfcbebbbe",
+    "url": "/static/css/loadedContent.css"
+  },
+  {
+    "revision": "251906cb07d00d841239",
+    "url": "/static/js/app.js"
+  },
+  {
+    "revision": "966cacce8c7a4f8a289a",
+    "url": "/static/js/background.js"
+  },
+  {
+    "revision": "86cfe19df03be68f5df76418b14534b1",
+    "url": "/static/js/background.js.LICENSE"
+  },
+  {
+    "revision": "5721e8dc356888b5da5b",
+    "url": "/static/js/content.js"
+  },
+  {
+    "revision": "25f836e214c9671dc879377e86444d4a",
+    "url": "/static/js/content.js.LICENSE"
+  },
+  {
+    "revision": "d41a90f52de477f3b3d6",
+    "url": "/static/js/loadedBackground.js"
+  },
+  {
+    "revision": "7639e30c7ac70f0939a0955fb8a64e09",
+    "url": "/static/js/loadedBackground.js.LICENSE"
+  },
+  {
+    "revision": "23576bd9f2adfcbebbbe",
+    "url": "/static/js/loadedContent.js"
+  },
+  {
+    "revision": "252efdefe695d453e7480b9431aae44a",
+    "url": "/static/js/loadedContent.js.LICENSE"
+  }
+]);
